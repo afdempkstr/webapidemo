efee34312d7b3a97efaf3b7aa8476621e3c818b9
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-02-27 19:34:38 +0200

    Add project files.

---
+++ WebApiDemo/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiDemo.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Title { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public int PublicationYear { get; set; }
+    }
+}
